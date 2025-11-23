1f8b3fff3d575e986a9017d76765c0d07d36a2a1
Artur Costa <arturscosta2005@gmail.com>
2025-11-23 20:56:10 -0300

    Created the eventListener to change the canvas size

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 container = document.querySelector("#container");
+changeCanvas = document.querySelector("#canvas-size");
+curSize = document.querySelector("p");
+
+changeCanvas.addEventListener("click", () => {
+  num = Number(prompt("Choose a canvas size (ex: 16)"));
+  while (num <= 0 || num > 100 || Number.isNaN(num) || null || num == "") {
+    num = Number(prompt("Invalid number, choose a canvas size (ex: 16)"));
+  }
+  canvasSize = num * num;
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  createCanvas();
+});
 
 let num = 32;
 
 let canvasSize = num * num;
 
-for (i = 0; i < canvasSize; i++) {
-  newDiv = document.createElement("div");
-  newDiv.style.flex = `1 1 calc(100% / (${num}))`;
+function createCanvas() {
+  for (i = 0; i < canvasSize; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.style.flex = `1 1 calc(100% / (${num}))`;
 
-  newDiv.style.aspectRatio = "1/1";
+    newDiv.style.aspectRatio = "1/1";
 
-  newDiv.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
-  });
+    newDiv.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "red";
+    });
 
-  container.appendChild(newDiv);
+    container.appendChild(newDiv);
+    curSize.innerText = "Current size: " + num + "x" + num;
+  }
 }
+
+createCanvas();
+
+curSize.innerText = "Current size: " + num + "x" + num;
